5d3e03ded401b5c3829ec54407d55c858fbd6a97
blu3edge <37814681+blu3edge@users.noreply.github.com>
2018-04-22 19:37:26 -0300

    Add files via upload

---
+++ rutes/user.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var express = require('express');
+var UserController = require('../controllers/user');
+
+var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
+var md_admin = require('../middlewares/is_admin');
+
+api.get('/pruebas-del-controlador', md_auth.ensureAuth, UserController.pruebas);
+api.post('/register' ,  UserController.saveUser);
+api.post('/login' ,  UserController.login);
+api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
+
+module.exports = api;
