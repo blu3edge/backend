6b287dd14b513633fbcec83b94366d10f3f9396b
blu3edge <37814681+blu3edge@users.noreply.github.com>
2018-04-22 19:37:46 -0300

    Add files via upload

---
+++ services/jwt.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = 'qweqwe123';
+
+exports.createToken = function(user){
+	var payload = {
+		sub: user._id,
+		name: user.nombre,
+		surname: user.apellido,
+		email: user.email,
+		role: user.role,
+		iat: moment().unix(),
+		exp: moment().add(30, 'days').unix
+
+	};
+	return jwt.encode(payload, secret);
+};
