9d00e2f4c99dc9820a2b0ccdbb7b5acfe95227fd
blu3edge <37814681+blu3edge@users.noreply.github.com>
2018-04-22 19:37:05 -0300

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+  nombre: String,
+  apellido: String,
+  email: String,
+  password: String,
+  role: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
