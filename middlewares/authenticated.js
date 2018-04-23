f8ee28294dc2ceb02261b8254d921812eeb2d8d0
blu3edge <37814681+blu3edge@users.noreply.github.com>
2018-04-23 00:02:41 -0300

    Add files via upload

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -7,14 +7,14 @@ var secret = 'qweqwe123';
 exports.ensureAuth = function(req, res, next){
 if(!req.headers.authorization){
 	return res.status(403).send({message : ' La petición no tiene la cabecera de autenticación'});
-} 
+}
 
 	var token = req.headers.authorization.replace(/['"]+/g, '');
 
 	try{
 		var payload = jwt.decode(token, secret);
 
-		if(ppayload.exp <= moment().unix()){
+		if(payload.exp <= moment().unix()){
 			return res.status(401).send({
 				message: 'El token ha expirado'
 			});
