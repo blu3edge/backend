adeef4ce128e32e9ac282c2b828ceb21de0865ec
blu3edge <37814681+blu3edge@users.noreply.github.com>
2022-02-18 14:41:05 -0300

    actualizacion

--- middlewares/is_admin.js
+++ middlewares/is_admin.js
@@ -1,5 +1,5 @@
 'use strict'
-
+//middleware impide que un usuarios distinto a admin tenga acceso a algun servicio.
 exports.isAdmin = function(req, res, next){
 if(req.user.role != 'ROLE_ADMIN'){
 	return res.status(200).send({message: 'No tienes acceso a esta zona'});
